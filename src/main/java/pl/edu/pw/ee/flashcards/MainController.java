1a5c9c2515ac47bec4e7975d2847bdf045ba11bf
Szymon Poltorak <szymonpotorak@gmail.com>
2022-07-12 09:59:08 +0200

    Added creating tables

--- src/main/java/pl/edu/pw/ee/flashcards/MainController.java
+++ src/main/java/pl/edu/pw/ee/flashcards/MainController.java
@@ -3,12 +3,18 @@ package pl.edu.pw.ee.flashcards;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import pl.edu.pw.ee.flashcards.database.Connector;
 import pl.edu.pw.ee.flashcards.switcher.SceneSwitcher;
 
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 import static pl.edu.pw.ee.flashcards.switcher.FxmlUrls.*;
+import static pl.edu.pw.ee.flashcards.utils.TablesSQL.CARDSET;
+import static pl.edu.pw.ee.flashcards.utils.TablesSQL.FLASHCARD;
 
 public class MainController implements Initializable {
     @FXML
@@ -17,6 +23,7 @@ public class MainController implements Initializable {
     private Button learnButton;
     @FXML
     private Button manageSetButton;
+    private static final Logger logger = LoggerFactory.getLogger(MainController.class);
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -25,5 +32,22 @@ public class MainController implements Initializable {
         manageSetButton.setOnAction(event -> SceneSwitcher.switchToNewScene(MANAGE.getPath(), event));
 
         learnButton.setOnAction(event -> SceneSwitcher.switchToNewScene(CHOOSE.getPath(), event));
+
+        createNecessary();
+    }
+
+    public void createNecessary(){
+        try (var connector = Connector.establishConnection()) {
+            if (connector == null){
+                throw new SQLException();
+            }
+
+            try (var statement = connector.createStatement()){
+                statement.execute(FLASHCARD.getSql());
+                statement.execute(CARDSET.getSql());
+            }
+        } catch (SQLException exception) {
+            logger.error("There is a problem with creating necessary tables.", exception);
+        }
     }
 }
